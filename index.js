90c1bf898f01881e10afb343072fa88bee49d780
lukechenluwang@gmail.com <luke.w@bmyg.com.au>
2019-02-18 22:52:07 +1100

    use nconf to control config

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const express = require('express');
+const nconf = require('nconf');
+
+nconf.argv()
+  .env()
+  .file({ file: './config.json' });
+
+const app = express();
+const port = nconf.get("port");
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
