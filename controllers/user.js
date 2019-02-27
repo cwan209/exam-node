07c3ae10a4ecf2a83c9a843527542126c8790659
lukechenluwang@gmail.com <luke.w@bmyg.com.au>
2019-02-27 22:14:50 +1100

    added bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,19 +1,23 @@
 const User = require('../database/schemas/user');
+const bcrypt = require('bcrypt');
+
 const signup = async function (req, res) {
 
   const {email, password} = req.body;
+  const saltRounds = 10;
 
   try {
     const user = await User.findOne({ email: email});
     if (user) return res.status(400).send(user);
 
-    const newUser = new User({ email: email, password:  password});
+    const hashedPassword = await bcrypt.hashSync(password, saltRounds);
+    const newUser = new User({ email: email, password: hashedPassword});
     const result = await newUser.save();
+
     return res.status(201).send({
       success: true,
       result: result
     });
-
   } catch (error) {
     return res.status(500).send(error);
   }
