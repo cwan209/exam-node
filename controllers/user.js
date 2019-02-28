b7159b591e98d908894068b05b5cbfb426104d35
lukechenluwang@gmail.com <luke.w@bmyg.com.au>
2019-02-28 21:54:05 +1100

    update API  signup

--- controllers/user.js
+++ controllers/user.js
@@ -8,7 +8,7 @@ const signup = async function (req, res) {
 
   try {
     const user = await User.findOne({ email: email});
-    if (user) return res.status(400).send(user);
+    if (user) return res.status(400).send(`User ${email} already exist`);
 
     const hashedPassword = await bcrypt.hashSync(password, saltRounds);
     const newUser = new User({ email: email, password: hashedPassword});
