5e1a831f24d8fa849b87548469d927966c83e407
lukechenluwang@gmail.com <luke.w@bmyg.com.au>
2019-03-17 18:04:56 +1100

    added file session;

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const nconf = require('nconf');
 const router = require('./router');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-var cors = require('cors');
+const cors = require('cors');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 nconf.argv()
   .env()
@@ -15,10 +17,26 @@ require("./database/db");
 
 const app = express();
 const port = nconf.get("port");
+const env = nconf.get("development");
 
 app.use(cors());
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Add session
+const expressSession = require('express-session');
+if (env === 'development') {
+  const FileStore = require('session-file-store')(expressSession);
+  app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'miloandfan',
+    store: new FileStore()
+  }));
+} else {
+
+}
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/api/v1',  router);
